3750bfb8b983c90eb3c3381b0484860341a7bbc2
Hamide Bay <hamide.bay@baycom.ch>
2021-03-29 00:57:32 +0200

    first-commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// 1. Component und Service Folder erstellen
+// 2. table.js und excell.js erstellen
+// 3. Excel lesen
+// 4. In Tabelle schreiben und ausgeben.
+
+import logo from './logo.svg';
+import './App.css';
+import Table from './component/table'
+
+function App() {
+  return (
+    <div className="App">
+     <Table/>
+    </div>
+  );
+}
+
+export default App;
